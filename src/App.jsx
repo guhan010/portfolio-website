8a2182b3dce2811cb98effbc45220bc66ad30fff
guhan010 <guhaneswaran6@gmail.com>
2023-09-12 19:33:31 +0530

    Created a react+vite app, Added Routes, Created and designed Navigation Bar, Installed MaterialUI, React-Router-Dom and required dependencies

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/resume" element={<Resume />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        tenny
+      </Link>
+      <ul>
+        <CustomLink to="/about">about</CustomLink>
+        <CustomLink to="/portfolio">portfolio</CustomLink>
+        <CustomLink to="/blog">blog</CustomLink>
+        <CustomLink to="/resume">resume</CustomLink>
+        <CustomLink to="/contact">contact</CustomLink>
+      </ul>
+    </nav>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
+
+CustomLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
