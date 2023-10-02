f74c7d434f5224880cd3f50abd7c70c9a72b28fb
guhan010 <guhaneswaran6@gmail.com>
2023-10-02 15:05:39 +0530

    added animation to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,7 @@ import {
 import MenuIcon from "@mui/icons-material/Menu";
 import { NavLink } from "react-router-dom";
 import "../styles/NavStyles.css";
+import { Transition } from "react-transition-group";
 
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -58,57 +59,68 @@ const NavBar = () => {
   return (
     <>
       <Box>
-        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
-          <Toolbar>
-            {/* MenuIcon aligned to the right for mobile */}
-            <Box
+        <Transition in={true} timeout={500} appear={true}>
+          {(state) => (
+            <AppBar
+              component={"nav"}
               sx={{
-                flexGrow: 1,
-                display: { xs: "block", sm: "none" },
+                bgcolor: "black",
+                transform: `translateY(${state === "entered" ? 0 : "-100%"})`,
+                transition: "transform 0.5s",
               }}
             >
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                edge="end" // Align the icon to the right
-                onClick={handleDrawerToggle}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Box>
+              <Toolbar>
+                {/* MenuIcon aligned to the right for mobile */}
+                <Box
+                  sx={{
+                    flexGrow: 1,
+                    display: { xs: "block", sm: "none" },
+                  }}
+                >
+                  <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="end" // Align the icon to the right
+                    onClick={handleDrawerToggle}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                </Box>
 
-            <Typography
-              color={"gray"}
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, ml: 3 }}
-            >
-              <NavLink className="home" to={"/"}>
-                <em>tenny</em>
-              </NavLink>
-            </Typography>
+                <Typography
+                  color={"gray"}
+                  variant="h6"
+                  component="div"
+                  sx={{ flexGrow: 1, ml: 3 }}
+                >
+                  <NavLink className="home" to={"/"}>
+                    <em>tenny</em>
+                  </NavLink>
+                </Typography>
 
-            <Box sx={{ display: { xs: "none", sm: "block" } }}>
-              <ul className="navigation-menu">
-                <li>
-                  <NavLink to={"/about"}>about</NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/portfolio"}>portfolio</NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/blog"}>blog</NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/resume"}>resume</NavLink>
-                </li>
-                <li>
-                  <NavLink to={"/contact"}>contact</NavLink>
-                </li>
-              </ul>
-            </Box>
-          </Toolbar>
-        </AppBar>
+                <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                  <ul className="navigation-menu">
+                    <li>
+                      <NavLink to={"/about"}>about</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to={"/portfolio"}>portfolio</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to={"/blog"}>blog</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to={"/resume"}>resume</NavLink>
+                    </li>
+                    <li>
+                      <NavLink to={"/contact"}>contact</NavLink>
+                    </li>
+                  </ul>
+                </Box>
+              </Toolbar>
+            </AppBar>
+          )}
+        </Transition>
 
         <Box component="nav">
           <Drawer
