57b6efc81376d4c4dc4ad391733e44045fd85c99
guhan010 <guhaneswaran6@gmail.com>
2023-10-02 16:07:57 +0530

    optimizing: added animation to intro

--- src/components/About1.jsx
+++ src/components/About1.jsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Slide, Typography } from "@mui/material";
 import about from "../assets/about.webp";
 
 function About1() {
@@ -6,68 +6,71 @@ function About1() {
     <div style={{ flexGrow: 1, padding: 16 }}>
       <Grid container spacing={2} justifyContent="center" marginY="40px">
         {/* Single Grid Container for Grid 1 and Grid 2 */}
-        <Grid item xs={12} sm={6} md={6}>
-          <Box
-            style={{
-              padding: "10px",
-              backgroundColor: "whitesmoke",
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
-            }}
-          >
-            <Typography
-              variant="h1"
-              className="MuiTypography--h1 MuiTypography-responsiveFontSm"
-              sx={{ fontSize: "80px", fontWeight: "700", color: "#333333" }}
+        <Slide in={true} direction="right" timeout={700}>
+          <Grid item xs={12} sm={6} md={6}>
+            <Box
+              style={{
+                padding: "10px",
+                backgroundColor: "whitesmoke",
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content
+              }}
             >
-              about..
-            </Typography>
-          </Box>
-          <Box
-            style={{
-              padding: 10,
-              backgroundColor: "whitesmoke",
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
-            }}
-          >
-            <Typography variant="body1" sx={{ fontSize: "15px" }}>
-              Hi.. I&apos;m a Full Stack Developer based in India.
-            </Typography>
-          </Box>
-          <Box
-            style={{
-              padding: 10,
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content,
-            }}
-          >
-            <Typography
-              variant="body1"
-              sx={{ marginY: "8px", fontSize: "15px" }}
+              <Typography
+                variant="h1"
+                className="MuiTypography--h1 MuiTypography-responsiveFontSm"
+                sx={{ fontSize: "80px", fontWeight: "700", color: "#333333" }}
+              >
+                about..
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                padding: 10,
+                backgroundColor: "whitesmoke",
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content
+              }}
             >
-              I channel my meticulousness, passion for creation, and tireless
-              commitment towards a purpose-driven work ethic. This fuels my
-              eagerness to create a significant impact within a thriving
-              organization, ultimately contributing to the transformation of our
-              world.
-            </Typography>
-          </Box>
-        </Grid>
+              <Typography variant="body1" sx={{ fontSize: "15px" }}>
+                Hi.. I&apos;m a Full Stack Developer based in India.
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                padding: 10,
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content,
+              }}
+            >
+              <Typography
+                variant="body1"
+                sx={{ marginY: "8px", fontSize: "15px" }}
+              >
+                I channel my meticulousness, passion for creation, and tireless
+                commitment towards a purpose-driven work ethic. This fuels my
+                eagerness to create a significant impact within a thriving
+                organization, ultimately contributing to the transformation of
+                our world.
+              </Typography>
+            </Box>
+          </Grid>
+        </Slide>
 
-        {/* Third Grid Item */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Box style={{ padding: 16, textAlign: "center" }}>
-            <img
-              src={about}
-              alt="Profile Picture"
-              style={{ width: "100%", height: "auto", borderRadius: "8px" }}
-            />
-          </Box>
-        </Grid>
+        <Slide in={true} direction="left" timeout={700}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Box style={{ padding: 16, textAlign: "center" }}>
+              <img
+                src={about}
+                alt="Profile Picture"
+                style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+              />
+            </Box>
+          </Grid>
+        </Slide>
       </Grid>
       <Divider variant="middle" />
     </div>
--- src/components/Contact1.jsx
+++ src/components/Contact1.jsx
@@ -1,4 +1,11 @@
-import { Box, Container, Divider, Grid, Icon, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Slide,
+  Typography,
+} from "@mui/material";
 import about from "../assets/about.webp";
 import twitter from "../assets/twitter.png";
 import linkedin from "../assets/linkedin.png";
@@ -9,168 +16,172 @@ const Contact1 = () => {
   return (
     <div style={{ flexGrow: 1, padding: 16 }}>
       <Grid container spacing={2} justifyContent="center" marginY="40px">
-        <Grid item xs={12} sm={6} md={6}>
-          <Box
-            style={{
-              padding: "10px",
-              backgroundColor: "whitesmoke",
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
-            }}
-          >
-            <Typography
-              variant="h1"
-              className="MuiTypography--h1 MuiTypography-responsiveFontSm"
-              sx={{ fontSize: "80px", fontWeight: "700", color: "#333333" }}
+        <Slide in={true} direction="right" timeout={700}>
+          <Grid item xs={12} sm={6} md={6}>
+            <Box
+              style={{
+                padding: "10px",
+                backgroundColor: "whitesmoke",
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content
+              }}
             >
-              contact..
-            </Typography>
-          </Box>
-          <Box
-            style={{
-              padding: 16,
-              backgroundColor: "whitesmoke",
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
-            }}
-          >
-            <Typography variant="body1" sx={{ fontSize: "15px" }}>
-              Get in touch with me via social media
-              <br /> or email.
-            </Typography>
-          </Box>
-          <Box
-            style={{
-              padding: 10,
-              textAlign: "left",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content,
-            }}
-          >
-            <Container>
-              <Grid container spacing={2}>
-                {/* Box 1 */}
-                <Grid item xs={12} sm={12} md={6} lg={6} maxWidth="50%">
-                  <a
-                    href="https://www.linkedin.com/in/guhan-eswaran-32a5ba254/"
-                    className="linkedin"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Box
-                      className="social"
-                      display="flex"
-                      alignItems="center"
-                      gap={1}
+              <Typography
+                variant="h1"
+                className="MuiTypography--h1 MuiTypography-responsiveFontSm"
+                sx={{ fontSize: "80px", fontWeight: "700", color: "#333333" }}
+              >
+                contact..
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                padding: 16,
+                backgroundColor: "whitesmoke",
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content
+              }}
+            >
+              <Typography variant="body1" sx={{ fontSize: "15px" }}>
+                Get in touch with me via social media
+                <br /> or email.
+              </Typography>
+            </Box>
+            <Box
+              style={{
+                padding: 10,
+                textAlign: "left",
+                maxWidth: "400px", // Adjust the maximum width as needed
+                margin: "0 auto", // Center align the content,
+              }}
+            >
+              <Container>
+                <Grid container spacing={2}>
+                  {/* Box 1 */}
+                  <Grid item xs={12} sm={12} md={6} lg={6} maxWidth="50%">
+                    <a
+                      href="https://www.linkedin.com/in/guhan-eswaran-32a5ba254/"
+                      className="linkedin"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <img
-                        src={linkedin}
-                        style={{ height: "30px", width: "30px" }}
-                      />
-                      LinkedIn
-                    </Box>
-                  </a>
-                </Grid>
+                      <Box
+                        className="social"
+                        display="flex"
+                        alignItems="center"
+                        gap={1}
+                      >
+                        <img
+                          src={linkedin}
+                          style={{ height: "30px", width: "30px" }}
+                        />
+                        LinkedIn
+                      </Box>
+                    </a>
+                  </Grid>
 
-                {/* Box 2 */}
-                <Grid item xs={12} sm={12} md={6} lg={6} maxWidth="50%">
-                  <a
-                    href="https://github.com/guhan010"
-                    className="github"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Box
-                      className="social"
-                      display="flex"
-                      alignItems="center"
-                      gap={1}
+                  {/* Box 2 */}
+                  <Grid item xs={12} sm={12} md={6} lg={6} maxWidth="50%">
+                    <a
+                      href="https://github.com/guhan010"
+                      className="github"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <img
-                        src={github}
-                        style={{ height: "30px", width: "30px" }}
-                      />
-                      Github
-                    </Box>
-                  </a>
-                </Grid>
+                      <Box
+                        className="social"
+                        display="flex"
+                        alignItems="center"
+                        gap={1}
+                      >
+                        <img
+                          src={github}
+                          style={{ height: "30px", width: "30px" }}
+                        />
+                        Github
+                      </Box>
+                    </a>
+                  </Grid>
 
-                {/* Box 3 */}
-                <Grid
-                  item
-                  xs={12}
-                  sm={12}
-                  md={6}
-                  lg={6}
-                  maxWidth="50%"
-                  marginY={2}
-                >
-                  <a
-                    href="https://twitter.com/__picasso___"
-                    className="twitter"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                  {/* Box 3 */}
+                  <Grid
+                    item
+                    xs={12}
+                    sm={12}
+                    md={6}
+                    lg={6}
+                    maxWidth="50%"
+                    marginY={2}
                   >
-                    <Box
-                      className="social"
-                      display="flex"
-                      alignItems="center"
-                      gap={1}
+                    <a
+                      href="https://twitter.com/__picasso___"
+                      className="twitter"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <img
-                        src={twitter}
-                        style={{ height: "30px", width: "30px" }}
-                      />
-                      Twitter
-                    </Box>
-                  </a>
-                </Grid>
+                      <Box
+                        className="social"
+                        display="flex"
+                        alignItems="center"
+                        gap={1}
+                      >
+                        <img
+                          src={twitter}
+                          style={{ height: "30px", width: "30px" }}
+                        />
+                        Twitter
+                      </Box>
+                    </a>
+                  </Grid>
 
-                {/* Box 4 */}
-                <Grid
-                  item
-                  xs={12}
-                  sm={12}
-                  md={6}
-                  lg={6}
-                  maxWidth="50%"
-                  marginY={2}
-                >
-                  <a
-                    href="https://www.instagram.com/_da_vinci.__/"
-                    className="instagram"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                  {/* Box 4 */}
+                  <Grid
+                    item
+                    xs={12}
+                    sm={12}
+                    md={6}
+                    lg={6}
+                    maxWidth="50%"
+                    marginY={2}
                   >
-                    <Box
-                      className="social"
-                      display="flex"
-                      alignItems="center"
-                      gap={1}
+                    <a
+                      href="https://www.instagram.com/_da_vinci.__/"
+                      className="instagram"
+                      target="_blank"
+                      rel="noopener noreferrer"
                     >
-                      <img
-                        src={instagram}
-                        style={{ height: "30px", width: "30px" }}
-                      />
-                      Instagram
-                    </Box>
-                  </a>
+                      <Box
+                        className="social"
+                        display="flex"
+                        alignItems="center"
+                        gap={1}
+                      >
+                        <img
+                          src={instagram}
+                          style={{ height: "30px", width: "30px" }}
+                        />
+                        Instagram
+                      </Box>
+                    </a>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </Container>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Box style={{ padding: 16, textAlign: "center" }}>
-            <img
-              src={about}
-              alt="Profile Picture"
-              style={{ width: "100%", height: "auto", borderRadius: "8px" }}
-            />
-          </Box>
-        </Grid>
+              </Container>
+            </Box>
+          </Grid>
+        </Slide>
+        <Slide in={true} direction="left" timeout={700}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Box style={{ padding: 16, textAlign: "center" }}>
+              <img
+                src={about}
+                alt="Profile Picture"
+                style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+              />
+            </Box>
+          </Grid>
+        </Slide>
       </Grid>
       <Divider variant="middle" />
     </div>
