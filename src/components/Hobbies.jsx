27879ea48c9f76d75bc8b442fd1c56c4719e49a1
guhan010 <guhaneswaran6@gmail.com>
2023-10-04 14:09:44 +0530

    modified hobbies

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,16 +1,16 @@
 import { Box, Divider, Grid, Typography } from "@mui/material";
-import about from "../assets/about.webp";
+import hobbies from "../assets/hobbies.jpg";
 
-function About1() {
+function Hobbies() {
   return (
     <div style={{ flexGrow: 1, padding: 16 }}>
       <Grid container spacing={2} justifyContent="space-evenly" marginY="50px">
         <Grid item xs={12} sm={6} md={4}>
           <Box style={{ padding: 16, textAlign: "center" }}>
             <img
-              src={about}
+              src={hobbies}
               alt="Profile Picture"
-              style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+              style={{ width: "120%", height: "auto", borderRadius: "8px" }}
             />
           </Box>
         </Grid>
@@ -21,53 +21,44 @@ function About1() {
               backgroundColor: "whitesmoke",
               textAlign: "center",
               maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
+              margin: "0 100px", // Center align the content
             }}
           >
             <Typography
               variant="h4"
-              className="MuiTypography--h1 MuiTypography-responsiveFontSm"
-              sx={{ fontSize: "30px", fontWeight: "700", color: "#333333" }}
+              className="regular-font"
+              sx={{ fontSize: "40px", textAlign: "left", color: "#333333" }}
             >
-              Hobbies and Interestes
-            </Typography>
-          </Box>
-          <Box
-            style={{
-              padding: 10,
-              backgroundColor: "whitesmoke",
-              maxWidth: "400px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content
-            }}
-          >
-            <Typography
-              variant="body1"
-              textAlign="left"
-              sx={{ fontSize: "15px" }}
-            >
-              Techie with an hobby of building and optimizing computer systems,
-              crafting high-performance PCs tailored to perfection
+              Self Insights
             </Typography>
           </Box>
           <Box
             style={{
               padding: 10,
               textAlign: "left",
-              maxWidth: "300px", // Adjust the maximum width as needed
-              margin: "0 auto", // Center align the content,
+              maxWidth: "400px", // Adjust the maximum width as needed
+              margin: "0 100px", // Center align the content,
             }}
           >
-            <Typography
-              variant="body1"
-              sx={{ marginTop: "8px", fontSize: "14px" }}
-            >
-              - I drink a lot of tea <br />
-              - I&apos;m in love with Ocean
-              <br />
-              - Exercising is my zen time <br />- Bit of Tech Addicted
-              <br />
-              - I&apos;m a bit of a clean freak
-              <br />
+            <Typography variant="body1" sx={{ fontSize: "14px" }}>
+              <ul className="hobbies">
+                <li style={{ marginBottom: "10px" }}>
+                  Tech&apos;s my thing—I&apos;m a PC enthusiast and techie
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  I enjoy coffee more frequently
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  Exercising is my zen time
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  I&apos;m passionate about exploring the wonders of the ocean
+                </li>
+                <li style={{ marginBottom: "10px" }}>
+                  Cleanliness is a part of my daily routine; it&apos;s like
+                  second natur
+                </li>
+              </ul>
             </Typography>
           </Box>
         </Grid>
@@ -77,4 +68,4 @@ function About1() {
   );
 }
 
-export default About1;
+export default Hobbies;
